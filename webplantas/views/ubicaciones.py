20929c774670a5abaebf8dbff9c857e672a23c58
DougO28 <douguiortega@gmail.com>
2026-02-11 16:19:39 -0600

    commit

--- webplantas/views/ubicaciones.py
+++ webplantas/views/ubicaciones.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets
-from rest_framework.permissions import AllowAny  # CAMBIAR ESTE IMPORT
+from rest_framework.permissions import AllowAny  
 from django_filters.rest_framework import DjangoFilterBackend
 
 from webplantas.models import Departamento, Municipio
